982872f77333ad7b8b2a07b905a49105f6409245
soleiyu <39758438+soleiyu@users.noreply.github.com>
2019-03-09 12:39:27 +0900

    Update vd.sh

--- vd.sh
+++ vd.sh
@@ -4,7 +4,7 @@ ffmpeg -i $1"f.mp4" -vf transpose=3 -metadata:s:v:0 rotate=0 $1"fr".mp4
 ffmpeg -i $1"r.mp4" -vf transpose=3 -metadata:s:v:0 rotate=0 $1"rr".mp4
 ffmpeg -i $1"s.mp4" -vf transpose=3 -metadata:s:v:0 rotate=0 $1"sr".mp4
 
-ffmpeg	-i $1"sr.mp4" -i $1"fr.mp4" -i $1"sr.mp4" -filter_complex "
+ffmpeg	-i $1"sr.mp4" -i $1"fr.mp4" -i $1"rr.mp4" -filter_complex "
 		nullsrc=size=3240x1920 [base];
 		[0:v] setpts=PTS-STARTPTS, scale=1080x1920 [left];
 		[1:v] setpts=PTS-STARTPTS, scale=1080x1920 [center];
