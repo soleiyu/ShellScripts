237c6bf043ddc71a8ff05a0e1beb319eaac4bf83
soleiyu <39758438+soleiyu@users.noreply.github.com>
2019-03-21 22:14:21 +0900

    Add files via upload

---
+++ encRuncher.sh
@@ -0,0 +1,15 @@
+echo Begin
+
+echo $#
+
+for x in "$@"
+do
+  org=${x%.*}
+	nmov=$org".mov"
+	nmp4=$org".mp4"
+	echo $nmov " -> " $nmp4
+
+	ffmpeg -i $nmov -b:v 25M -c:v h264_nvenc -vf transpose=1 $nmp4
+done
+
+echo DONE
